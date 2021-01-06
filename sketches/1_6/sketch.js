30d7e0fb0823eb4edb8f0c37acddb40998787e77
ninalutz <nlutz@mit.edu>
2021-01-06 14:30:41 -0500

    add 1/6

---
+++ sketches/1_6/sketch.js
@@ -0,0 +1,54 @@
+/*
+
+FORKED FROM 
+ * twisted lines
+ *
+ * @author aadebdeb
+ * @date 2017/02/04
+
+ Fork and modifications by Nina Lutz 2020 and 2021
+ */
+
+var colors;
+var type;
+
+function setup() {
+  var canvas;
+  if(windowWidth>=600 && windowHeight >=600){
+    canvas = createCanvas(600, 600);
+  }
+  else{
+    canvas = createCanvas(windowWidth, windowHeight - 100);
+  }
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
+  canvas.position(x, y);
+}
+
+function draw() {
+
+  blendMode(BLEND);
+  background(0, 50)
+  fill(255);
+  noStroke()
+  text("1.6.21", 30, height - 30)
+  blendMode(EXCLUSION);
+  noFill();
+  strokeWeight(15);
+
+  for(var i = 0; i < 3; i++) {
+    for(var j = 0; j < 10; j++) {
+    beginShape();
+    stroke(i*j, j*3, j*20 + i*30, 20);
+    for(var w = -20; w < width + 20; w += 6) {
+      var h = 250;
+      h += 100 * sin(w * 0.03 + frameCount * 0.07 + i * TWO_PI / 3) * pow(abs(sin(w * 0.001 + frameCount * 0.02)), 5);
+      ellipse(h + j*10, w, 50, 50);
+    }    
+    endShape();
+  }
+  }
+  
+}
