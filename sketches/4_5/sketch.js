6bdeb08544188a3e45cc7b4f2009c13f4f5db486
ninalutz <nlutz@mit.edu>
2021-04-05 11:06:11 -0400

    add sketch

---
+++ sketches/4_5/sketch.js
@@ -0,0 +1,76 @@
+//Inspo from
+/*
+200504 Spiral Storm
+by CHE YU WU 
+*/
+let colors = "ee6055-60d394-ffd97d-aaf683-edf67d-ff9b85".split("-").map(a=>"#"+a);
+let overAllTexture;
+
+function setup() {
+
+  fill('black')
+  
+  overAllTexture=createGraphics(width,height)
+  
+  overAllTexture.loadPixels()
+  for(var i=0;i<width;i++){
+    for(var o=0;o<height;o++){
+      overAllTexture.set(i,o,color(100,noise(i/3,o/3,i*o/50)*random([0,50,100])))
+    }
+  }
+  overAllTexture.updatePixels()
+
+  w = 600;
+    var canvas;
+    if(windowWidth>=600 && windowHeight >=600){
+      canvas = createCanvas(600, 600);
+    }
+    else{
+      canvas = createCanvas(windowWidth, windowHeight - 100);
+    }
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
+  canvas.position(x, y);
+  background(255);
+  noStroke();
+
+}
+
+
+function draw() {
+  background(255, 10)
+  fill(0,0,20)
+  noStroke()
+  push()
+    rotate(PI/4)
+    stroke(255)
+    strokeWeight(3)
+
+    for(var i=0;i<1000;i+=0.5){
+      let clr = color(colors[ int(i/(300/50+10))%colors.length])
+      if (i%50<20){
+        clr.setAlpha(100)
+        strokeWeight(1)
+      }
+      stroke(clr)
+      let freq = 4+sin(frameCount/1000)/10+ 500/100+0.2+ noise(i/500,frameCount/200)*2
+      let rr =  + sin(i/250+frameCount/500)*450 
+
+
+      let sang = i/freq+frameCount/20
+      let eang = sang + 1/freq
+      let sx = i-1+cos(sang)*rr/10
+      let sy = sin(sang)*rr
+      let ex = i+cos(eang)*rr/20
+      let ey = sin(eang)*rr
+
+     line(sx,sy,ex,ey)
+     // fill(clr)
+    // rect(sx, sy, 1, 1)
+    }
+  
+  pop()
+
+  }
