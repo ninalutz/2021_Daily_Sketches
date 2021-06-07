401fe2126137275f1d1196c2b75ed6656e7d6f57
ninalutz <nlutz@mit.edu>
2021-06-07 09:20:50 -0400

    add sketch

---
+++ sketches/6_7/sketch.js
@@ -0,0 +1,44 @@
+//inspo from Fiona 
+
+let moverSystems = [];
+let num = 10;
+let cells, cols, rows;
+let offset, margin;
+let cellW, cellH;
+let bg;
+let graphics;
+
+
+
+function setup() {
+  w = 600;
+  var canvas;
+    if(windowWidth>=600 && windowHeight >=600){
+      canvas = createCanvas(600, 600);
+    }
+    else{
+      canvas = createCanvas(windowWidth, windowHeight - 100);
+    }
+
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+
+  canvas.position(x, y);
+  background(0);
+  noStroke();
+
+}
+var t = 0;
+
+function draw(){
+  background(0, 20)
+  t+=1/500
+  fill(255, 100)
+  for(var x=100;x<width;x+=100){
+    for(var y=-40;y<height+40;y++){
+      var a=noise(x,y/width,t)*10
+    　var b=noise(x,y/width-t)*300
+       ellipse(x+sin(b)*(cos(a)-sin(x|y))*20,y+sin(a)*500, 2, 2)
+     }
+  }
+}
